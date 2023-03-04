51ae07f9145630a29c2ad322895d0e76e8292703
Bpar_Hapoda <cddvdrom@yandex.ru>
2023-03-04 20:23:27 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,15 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Введите число :");
         Scanner scanner = new Scanner(System.in);
-        int n=scanner.nextInt();
-        System.out.println("Сумма чисел от 1 до "+n+" равна "+sum(5));
-        System.out.println("Произведение чисел от 1 до "+n +" равно "+fact(3));
+        int n = scanner.nextInt();
+        System.out.println("Сумма чисел от 1 до " + n + " равна " + sum(5));
+        System.out.println("Произведение чисел от 1 до " + n + " равно " + fact(3));
+        System.out.println("Простые числа от 1 до 1000 :");
+        System.out.println(simple(1000));
     }
 
     public static int sum(int n) {
@@ -26,4 +29,26 @@ public class Main {
         fact = fact(n - 1) * n;
         return fact;
     }
+
+    public static ArrayList<Integer> simple(int n) {
+        ArrayList<Integer> result = new ArrayList<>();
+        int count;
+        for (int i = 0; i <= n; i++) {
+            count = 0;
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0) {
+                    count++;
+                }
+                if (count > 2) {
+                    break;
+                }
+            }
+            if (count == 2) {
+                result.add(i);
+            }
+        }
+        return result;
+    }
+
+
 }
